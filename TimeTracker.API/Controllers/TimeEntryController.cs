83f44733cf763d8876c263f90b69153845c528f3
“jujo2594” <105889688+jujo2594@users.noreply.github.com>
2025-01-29 16:54:50 -0500

    Add project files.

---
+++ TimeTracker.API/Controllers/TimeEntryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Core.Entities;
+
+namespace TimeTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeEntryController : ControllerBase
+    {
+        private static List<TimeEntry> _timeEntries = new List<TimeEntry>
+        {
+            new TimeEntry
+            {
+                Id = 1,
+                Project = "Time tracker App",
+                End = DateTime.Now.AddHours(1)
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<TimeEntry>> GetAllTimeEntries()
+        {
+            return Ok(_timeEntries);
+        }
+    }
+}
---
+++ TimeTracker.Core/Entities/TimeEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Core.Entities
+{
+    public class TimeEntry
+    {
+        public int Id { get; set; }
+        //Colocando "required" hacemos el campo "Project" obligatorio, haciendo que cada vez que tengamos una entrada, debemos establecer un "Project"
+        public required string Project { get; set; }
+        public DateTime Start { get; set; } = DateTime.Now;
+        public DateTime? End{ get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime? DateUpdated { get; set; }
+    }
+}
