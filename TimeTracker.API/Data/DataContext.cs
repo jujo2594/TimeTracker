15b66000e61be85b0c40bdcd641ad159633b00b9
“jujo2594” <105889688+jujo2594@users.noreply.github.com>
2025-01-30 21:33:38 -0500

    Implement DbContext

---
+++ TimeTracker.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using TimeTracker.Core.Entities;
+
+namespace TimeTracker.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+    }
+}
